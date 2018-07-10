0a996ad7e7d2be6ec29600bdfc899cd3ee7544fe
dain7672 <dain7672@gmail.com>
2018-07-10 14:15:21 +0900

    step 3 구현

--- src/main/java/codesquad/web/QuestionController.java
+++ src/main/java/codesquad/web/QuestionController.java
@@ -2,11 +2,14 @@ package codesquad.web;
 
 import codesquad.web.domain.Question;
 import codesquad.web.domain.QuestionRepository;
+import codesquad.web.domain.User;
 import codesquad.web.util.Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/questions")
@@ -15,7 +18,8 @@ public class QuestionController {
     private QuestionRepository questionRepository;
 
     @PostMapping
-    public String create(Question question) {
+    public String create(Question question, HttpSession session) {
+        question.setWriter((User)session.getAttribute("sessionUser"));
         questionRepository.save(question);
         return "redirect:/";
     }
@@ -27,8 +31,14 @@ public class QuestionController {
     }
 
     @GetMapping("/{id}/form")
-    public String updateForm(@PathVariable long id, Model model) {
-        model.addAttribute("question", Util.findQuestionById(id, questionRepository));
+    public String updateForm(@PathVariable long id, Model model, HttpSession session) {
+        Question question = Util.findQuestionById(id, questionRepository);
+        User writer = question.getWriter();
+        if(!writer.equals((User)session.getAttribute("sessionUser"))) {
+            model.addAttribute("errorMessage", "다른 사람의 글은 수정할 수 없어..");
+            return "common/errorPage";
+        }
+        model.addAttribute("question", question);
         return "/qna/updateForm";
     }
 
@@ -41,8 +51,27 @@ public class QuestionController {
     }
 
     @DeleteMapping("/{id}")
-    public String delete(@PathVariable long id){
-        questionRepository.delete(Util.findQuestionById(id, questionRepository));
+    public String delete(@PathVariable long id, Model model, HttpSession session){
+        Question question = Util.findQuestionById(id, questionRepository);
+        User writer = question.getWriter();
+        User loginUser = (User)session.getAttribute("sessionUser");
+        if(loginUser == null){
+            return "users/loginForm";
+        }
+        if(!writer.equals(loginUser)) {
+            model.addAttribute("errorMessage", "다른 사람의 글은 삭제할 수 없어..");
+            return "common/errorPage";
+        }
+        questionRepository.delete(question);
         return "redirect:/";
+    }
+
+    @GetMapping("/form")
+    public String form(HttpSession session){
+        User loginUser = (User) session.getAttribute("sessionUser");
+        if (loginUser == null) {
+            return "redirect:/users/loginForm";
+        }
+        return "qna/form";
     }
 }
--- src/main/java/codesquad/web/UserController.java
+++ src/main/java/codesquad/web/UserController.java
@@ -3,16 +3,29 @@ package codesquad.web;
 import codesquad.web.domain.User;
 import codesquad.web.domain.UserRepository;
 import codesquad.web.util.Util;
+import netscape.security.ForbiddenTargetException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+
+    @PostMapping("/login")
+    public String login(String userId, String password, HttpSession session) {
+        Optional<User> maybeUser = userRepository.findByUserId(userId);
+        User user = maybeUser.orElseThrow(() -> new IllegalArgumentException("해당 아이디를 찾을 수 없습니다."));
+        if (user.matchPassword(password))
+            session.setAttribute("sessionUser", user);
+        return "redirect:/";
+    }
 
     @PostMapping
     public String create(User user, Model model) {
@@ -33,15 +46,25 @@ public class UserController {
     }
 
     @GetMapping("/{id}/form")
-    public String updateForm(@PathVariable long id,  Model model) {
-        model.addAttribute("user", Util.findUserById(id, userRepository));
+    public String updateForm(@PathVariable long id, Model model, HttpSession session) {
+        User user = Util.findUserById(id, userRepository);
+        if (!user.equals((User) session.getAttribute("sessionUser"))) {
+            model.addAttribute("errorMessage", "다른 사람의 정보는 수정할 수 없어..");
+            return "common/errorPage";
+        }
+        model.addAttribute("user", user);
         return "/user/updateForm";
     }
 
     @PutMapping("/{id}")
-    public String update(@PathVariable long id, User newUser){
+    public String update(@PathVariable long id, User newUser, HttpSession session) {
+        //내 정보만 수정할 수 있도록 변경한다.
+        User loginedUser = (User) session.getAttribute("sessionUser");
         User user = Util.findUserById(id, userRepository);
-        if (user.isEqualPassword(newUser)) {
+        if (!user.equals(loginedUser)) {
+            throw new ForbiddenTargetException("다른 사용자의 정보를 수정할 수 없습니다.");
+        }
+        if (user.matchPassword(newUser)) {
             user.update(newUser);
             userRepository.save(user);
         }
